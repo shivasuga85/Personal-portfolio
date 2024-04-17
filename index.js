f555f55844c8a364461f6495018e5f4c564d1ca3
shivasuga85 <153710706+shivasuga85@users.noreply.github.com>
2024-04-17 14:02:16 +0530

    1 commit

---
+++ index.js
@@ -0,0 +1,7 @@
+var typed =new typed(".multiple-text",{
+    Strings:["Front-developer","Front-End Developer","Front-End Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    Loop:true,
+});
